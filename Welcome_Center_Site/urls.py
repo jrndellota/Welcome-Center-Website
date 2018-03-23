9b65783858e4e4d13ed424eceb3a318fe232247c
Jaron Dellota <jrndellota@gmail.com>
2018-03-23 16:51:39 +0800

    added forms

--- Welcome_Center_Site/urls.py
+++ Welcome_Center_Site/urls.py
@@ -20,4 +20,5 @@ from first_app import views
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
     url(r'^admin/', admin.site.urls),
+    url(r'^first_app/read_qrcode/$', views.read_qrcode, name='read_qrcode')
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -2,9 +2,32 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import  JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from first_app.models import Attendance
+from first_app.forms import ImageForm
+
+import re
+import base64
+from PIL import Image
+import pyzbar.pyzbar as pyzbar
+
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html',)
+    form = ImageForm()
+    code ='this is the qrcode'
+
+    if request.method == 'POST' and request.FILES['img']:
+        myfile = request.FILES['img']
+        dataUrlPattern = re.compile('data:image/(png|jpeg);base64,(.*)$')
+
+        image_data = dataUrlPattern.match(myfile).group(2)
+        image_data = image_data.encode()
+        image_data = base64.b64decode(image_data)
+        image = Image.formarray(image_data)
+        code = pyzbar.decode(image).data
+        print(code)
+
+    return render(request, 'index.html',{"code":code, "form": form})
