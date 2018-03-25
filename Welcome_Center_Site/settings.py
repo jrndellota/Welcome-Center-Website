671b4a390f3408e165fe2d5b524f063aa780cf4e
Jaron Dellota <Jaron@Jarons-MacBook-Pro.local>
2018-03-25 22:24:09 +0800

    added qr reader

--- Welcome_Center_Site/settings.py
+++ Welcome_Center_Site/settings.py
@@ -15,8 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_DIR = os.path.join(BASE_DIR,'static')
-MEDIA_DIR = os.path.join(BASE_DIR,'media')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR,]
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 # MEDIA INFORMATION:
 MEDIA_ROOT = MEDIA_DIR
--- first_app/views.py
+++ first_app/views.py
@@ -2,33 +2,35 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import  JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 
 from first_app.models import Attendance
 from first_app.forms import ImageForm
 
 import re
 import base64
+import zbarlight
 from PIL import Image
-#from pyzbar.pyzbar import decode
+from io import BytesIO
 
 # Create your views here.
 
+
 def index(request):
     form = ImageForm()
-    code ='this is the qrcode'
+    code = 'this is the qrcode'
 
     if request.method == 'POST':
         data = ImageForm(request.POST)
-        print(data)
-        dataUrlPattern = re.compile('data:image/(png|jpeg);base64,(.*)$')
-        image_data= data.cleaned_data['img']
-        image_data = dataUrlPattern.match(image_data).group(2)
-        image_data = image_data.encode()
-        image_data = base64.b64decode(image_data)
-        #image = Image.open(image_data)
-        code = decode(image_data).data
-        print(code)
+        if data.is_valid():
+            dataUrlPattern = re.compile('data:image/(png|jpeg);base64,(.*)$')
+            image_data = data.cleaned_data['img']
+            image_data = dataUrlPattern.match(image_data).group(2)
+            image_data = image_data.encode()
+            image_data = base64.b64decode(image_data)
+            image = Image.open(BytesIO(image_data))
+            code = zbarlight.scan_codes('qrcode', image)
+            print(code)
+        else:
+            print('form is not valid')
 
-    return render(request, 'index.html',{"code":code, "form": form})
+    return render(request, 'index.html', {"code": code, "form": form})
